10da5a3ee294b3663e1042340c7b3d1e12d74d3b
stormhenkel <belos117@gmail.com>
2025-06-25 13:59:53 +0200

    Added POST

--- main.go
+++ main.go
@@ -22,10 +22,24 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	newTodo.ID = len(todos) + 1
+	todos = append(todos, newTodo)
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	r := gin.Default()
 
 	r.GET("/todos", getTodos)
+	r.POST("/todos", addTodo)
 
 	r.Run("localhost:8989") // Run on port 8989
 }
